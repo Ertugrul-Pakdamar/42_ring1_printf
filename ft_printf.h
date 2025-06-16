6eb479c69ed6bdc1e52e05459b54eea370bf744f
Ertugrul Lenovo Ubuntu Terminal <ertu.pakdamar@gmail.com>
2025-06-16 19:40:18 +0300

    ft_printf.h: update header timestamp and correct base definitions

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: epakdama <epakdama@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 11:29:10 by epakdama          #+#    #+#             */
-/*   Updated: 2025/06/16 19:22:59 by epakdama         ###   ########.fr       */
+/*   Updated: 2025/06/16 19:38:09 by epakdama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-# define UPPER_BASE "0123456789abcdef"
-# define LOWER_BASE "0123456789ABCDEF"
+# define UPPER_BASE "0123456789ABCDEF"
+# define LOWER_BASE "0123456789abcdef"
 
 int		ft_printf(const char *s, ...);
 void	ft_putstr(char *s, int *len);
