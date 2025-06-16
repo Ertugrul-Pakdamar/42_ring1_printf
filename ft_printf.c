db98ed8bd73615597b1904f72e9bcc144bd45024
Ertugrul Lenovo Ubuntu Terminal <ertu.pakdamar@gmail.com>
2025-06-16 19:47:57 +0300

    ft_printf.c: update timestamp and add FIXME comment for pointer handling

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: epakdama <epakdama@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 11:29:38 by epakdama          #+#    #+#             */
-/*   Updated: 2025/06/16 19:35:03 by epakdama         ###   ########.fr       */
+/*   Updated: 2025/06/16 19:47:47 by epakdama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,7 @@ static void	write_variadic(char *s, int i, va_list args, int *len)
 		ptr = va_arg(args, void *);
 		if (ptr == NULL)
 			ft_putstr("(nil)", len);
+		// FIXME:	nil yazıyor orijinaliyle aynı ama printf medicten geçmedi
 		else
 		{
 			ft_putstr("0x", len);
