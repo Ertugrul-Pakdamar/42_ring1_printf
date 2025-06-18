dad50d0a534c4dee7629ddcbf4c6b4b8139f896f
Ertugrul Pakdamar <ertu.pakdamar@gmail.com>
2025-06-18 09:53:17 +0300

    printf protorype added to header file

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: epakdama <epakdama@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 11:29:10 by epakdama          #+#    #+#             */
-/*   Updated: 2025/06/17 19:46:51 by epakdama         ###   ########.fr       */
+/*   Updated: 2025/06/18 09:52:39 by epakdama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@
 # define UPPER_BASE "0123456789ABCDEF"
 # define LOWER_BASE "0123456789abcdef"
 
+int		ft_printf(const char *s, ...);
 void	ft_putstr(char *s, int *len);
 void	ft_putchar(char c, int *len);
 void	ft_putnbr(int nb, int *len);
