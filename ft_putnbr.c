4efa8ed876a48c552b4c2670b067782dc67a9a14
Ertugrul Pakdamar <ertu.pakdamar@gmail.com>
2025-06-23 08:02:35 +0300

    unused headers removed

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -6,9 +6,11 @@
 /*   By: epakdama <epakdama@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 13:13:34 by epakdama          #+#    #+#             */
-/*   Updated: 2025/06/23 07:59:16 by epakdama         ###   ########.fr       */
+/*   Updated: 2025/06/23 08:01:32 by epakdama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "ft_printf.h"
 
 void	ft_putnbr(int nb, int *len)
 {
--- ft_putunsigned.c
+++ ft_putunsigned.c
@@ -6,9 +6,11 @@
 /*   By: epakdama <epakdama@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 13:15:58 by epakdama          #+#    #+#             */
-/*   Updated: 2025/06/23 07:59:40 by epakdama         ###   ########.fr       */
+/*   Updated: 2025/06/23 08:01:38 by epakdama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "ft_printf.h"
 
 void	ft_putunsigned(unsigned int nb, int *len)
 {
